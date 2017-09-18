16bf9fdd85af08603d0f8d8dc64c73d64e917927
vinimarcili <vinipetter@gmail.com>
2017-09-18 09:03:56 -0300

    Fix Mobile Menu problem

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
   <div id="app" class="default-font">
     <header>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="/">
-              Easy Order
-            </a>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" aria-expanded="false">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-          </div>
-          <div class="collapse navbar-collapse" id="menu">
-            <ul class="nav navbar-nav navbar-right">
-              <li class="active">
-                <a href="/pedidos">
-                  Pedidos
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  Clientes
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  Logout
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <b-navbar toggle-breakpoint="sm" toggleable="sm">
+        <b-navbar-brand class="transition" href="#">EasyOrder</b-navbar-brand>
+        <b-nav-toggle target="menu" class="transition">
+          <i class="fa fa-bars" aria-hidden="true"></i>
+        </b-nav-toggle>
+        <b-collapse position="right" is-nav id="menu">
+          <b-nav is-nav-bar>
+            <b-nav-item href="/pedidos">Pedidos</b-nav-item>
+            <b-nav-item href="#" disabled>Clientes</b-nav-item>
+            <b-nav-item href="#" disabled>Logout</b-nav-item>
+          </b-nav>
+        </b-collapse>
+      </b-navbar>
     </header>
     <main>
       <router-view></router-view>
@@ -58,5 +37,65 @@ export default {
 <style lang="scss">
 #app {
   margin: 0;
+  header {
+    nav {
+      .navbar-brand {
+        color: black;
+        &:hover {
+          background: black;
+          color: white;
+        }
+      }
+      #menu{
+        background: #eee;
+        .navbar-nav {
+          float: right;
+          li {
+            text-align: center;
+            a {
+              transition: all 0.3s ease;
+              color: black;
+              &:hover {
+                background: black;
+                color: white;
+              }
+            }
+          }
+        }
+      }
+      .navbar-toggler {
+        background: white;
+        border: 2px solid black;
+        border-radius: 5px;
+        color: black;
+        display: none;
+        float: right;
+        width: 40px;
+        height: 35px;
+        font-size: 23px;
+        position: relative;
+        right: 15px;
+        top: 5px;
+        &:hover {
+          background: black;
+          color: white;
+        }
+        &:focus {
+          outline: none;
+        }
+      }
+      @media (max-width: 767px) {
+        .navbar-toggler{
+          display: inline-block;
+        }
+        #menu{
+          .navbar-nav{
+            float: none;
+          }
+          clear: both;
+        }
+      }
+    }
+  }
 }
 </style>
